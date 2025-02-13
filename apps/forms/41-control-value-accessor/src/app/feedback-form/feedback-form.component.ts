6b6478accbf922175d3f9fca54369d3e7d90b088
Tetiana Zelenska <tz@techwhirl.io>
2025-02-13 01:28:34 +0100

    feat(41-control-value-accessor): solution

--- apps/forms/41-control-value-accessor/src/app/feedback-form/feedback-form.component.ts
+++ apps/forms/41-control-value-accessor/src/app/feedback-form/feedback-form.component.ts
@@ -25,15 +25,15 @@ export class FeedbackFormComponent {
     email: new FormControl('', {
       validators: Validators.required,
     }),
+    rating: new FormControl('', {
+      validators: Validators.required,
+    }),
     comment: new FormControl(),
   });
-
-  rating: string | null = null;
 
   submitForm(): void {
     this.feedBackSubmit.emit({
       ...this.feedbackForm.value,
-      rating: this.rating,
     });
 
     this.feedbackForm.reset();
--- apps/forms/41-control-value-accessor/src/app/rating-control/rating-control.component.ts
+++ apps/forms/41-control-value-accessor/src/app/rating-control/rating-control.component.ts
@@ -1,23 +1,53 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-rating-control',
   templateUrl: 'rating-control.component.html',
   styleUrls: ['rating-control.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RatingControlComponent),
+      multi: true,
+    },
+  ],
 })
-export class RatingControlComponent {
-  @Output()
-  readonly ratingUpdated: EventEmitter<string> = new EventEmitter<string>();
-
+export class RatingControlComponent implements ControlValueAccessor {
   value: number | null = null;
+  isDisabled = false;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onChange: (value: number | null) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onTouched: () => void = () => {};
 
   setRating(index: number): void {
+    if (this.isDisabled) return;
+
     this.value = index + 1;
-    this.ratingUpdated.emit(`${this.value}`);
+    this.onChange(this.value);
+    this.onTouched();
   }
 
-  isStarActive(index: number, value: number | null): boolean {
-    return value ? index < value : false;
+  isStarActive(index: number): boolean {
+    return this.value ? index < this.value : false;
+  }
+
+  writeValue(value: number | null): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: number | null) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 }
